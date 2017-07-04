857ede1ed63a9fc0f7b124b7726e1cf530c8a1be
5argon <pyasry@gmail.com>
2017-07-05 04:47:50 +0700

    Add some new functions

--- jidou-gdrive.sh
+++ jidou-gdrive.sh
@@ -1,4 +1,5 @@
 source "settings.sh";
 gdrive sync upload "$BMS_ZIP" $GDRIVE_BMS_FOLDER;
+gdrive share $GDRIVE_BMS_FOLDER;
 gdrive list --query "'$GDRIVE_BMS_FOLDER' in parents" --no-header > gdrive_list.txt;
 cat gdrive_list.txt;
